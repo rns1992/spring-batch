cbb4f861cc7b14177efade7521fa4a501e06f18c
Raj Soni <rajsoni.git@gmail.com>
2019-09-16 01:14:45 +0530

    Push initial structure for batch

---
+++ csv2json/src/main/java/com/example/csv2json/Csv2jsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.csv2json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csv2jsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csv2jsonApplication.class, args);
+	}
+
+}
---
+++ csv2json/src/test/java/com/example/csv2json/Csv2jsonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.csv2json;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Csv2jsonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
